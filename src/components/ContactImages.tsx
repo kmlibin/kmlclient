f65995ff09f4a1687ce72e8cdc10c98a6f2eab0f
Kelli Libin <km.putnam2@gmail.com>
2024-10-09 12:01:56 -0600

    working with jsparticle.

--- src/components/ContactImages.tsx
+++ src/components/ContactImages.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 //images
 
@@ -7,12 +7,144 @@ import kelli2 from "../../public/images/kelli2.png";
 import message from "../../public/images/mborder.png";
 //libraries
 import Image from "next/image";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 import { gsap } from "gsap";
+
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { loadSlim } from "tsparticles-slim";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+  Engine,
+} from "@tsparticles/engine";
+
+type ParticleProps = {
+  id: string;
+  position: { x: string; y: string };
+};
+
+const ParticleExplosion = ({ id }: ParticleProps) => {
+  const [init, setInit] = useState(false);
+
+  // Initialize tsParticles only once
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      //@ts-ignore
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#000000", // Set background color to black
+        },
+      },
+      fullScreen: {
+        enable: true,
+        zIndex: -1, // Set zIndex so that the particles appear behind other content
+      },
+      interactivity: {
+        detectsOn: "window", // Set interactivity detection to the whole window
+      },
+      emitters: {
+        position: {
+          x: 50, // Emitter position (centered horizontally)
+          y: 100, // Emitter position (near bottom)
+        },
+        rate: {
+          quantity: 5, // Emit 5 particles at a time
+          delay: 0.25, // 0.25s delay between emissions
+        },
+      },
+      particles: {
+        color: {
+          value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"], // Set particle colors
+        },
+        move: {
+          decay: 0.05, // Slow down particle speed over time
+          direction: "top", // Particles move upwards
+          enable: true, // Enable particle movement
+          gravity: {
+            enable: true, // Enable gravity effect on particles
+          },
+          outModes: {
+            top: "none", // Particles don't disappear at the top
+            default: "destroy", // Particles are destroyed when they move out of bounds
+          },
+          speed: { min: 25, max: 50 }, // Particle speed range
+        },
+        number: {
+          value: 0, // Initial particle number
+        },
+        opacity: {
+          value: 1, // Full opacity for particles
+        },
+        rotate: {
+          value: { min: 0, max: 360 }, // Rotation range
+          direction: "random", // Random rotation direction
+          animation: {
+            enable: true,
+            speed: 30, // Rotation speed
+          },
+        },
+        tilt: {
+          direction: "random", // Random tilt direction
+          enable: true, // Enable tilt
+          value: { min: 0, max: 360 }, // Tilt value range
+          animation: {
+            enable: true,
+            speed: 30, // Tilt speed
+          },
+        },
+        size: {
+          value: 8, // Set particle size
+        },
+        roll: {
+          darken: {
+            enable: true,
+            value: 25, // Darken effect intensity
+          },
+          enable: true, // Enable roll effect
+          speed: { min: 5, max: 15 }, // Roll speed range
+        },
+        wobble: {
+          distance: 30, // Wobble distance
+          enable: true, // Enable wobble
+          speed: { min: -7, max: 7 }, // Wobble speed range
+        },
+        shape: {
+          type: ["circle"], // Use circular shape for particles
+        },
+      },
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <Particles id={id} particlesLoaded={particlesLoaded} options={options} />
+    );
+  }
+
+  return null;
+};
 
 const ContactImages = () => {
-  const [isOrbiting, setIsOrbiting] = useState(false);
+  const [isParticleVisible, setIsParticleVisible] = useState(false);
+  const [particlePosition, setParticlePosition] = useState({ x: "0", y: "0" });
   gsap.registerPlugin(MotionPathPlugin);
+
   useEffect(() => {
     const animateElement = document.querySelector("#kelli-image");
 
@@ -35,83 +167,68 @@ const ContactImages = () => {
     );
   }, []);
 
-  // Click handler for orbit animation
-  const handleOrbitClick = () => {
+  const handleClick = () => {
     const animateElement = document.querySelector("#kelli-image");
-    if (isOrbiting) {
-      // stop animation if it's already orbiting
-      gsap.killTweensOf(animateElement);
-      setIsOrbiting(false);
-      gsap.to(animateElement, {
-        duration: 0,
-        //resets to resting position
-        motionPath: {
-          path: [{ x: 0, y: 0 }], // Reset position to final resting position
-          curviness: 0,
-        },
-      });
-    } else {
-      //start orbit
-      gsap.to(animateElement, {
-        duration: 3,
-        motionPath: {
-          path: [
-            { x: 5, y: 5 }, // bottom right
-            { x: -3, y: 3 }, // bottom left
 
-            { x: 0, y: 0 }, // rest position
-          ],
-          curviness: 2,
-        },
-        ease: "power1.inOut",
-        autoRotate: true,
-        repeat: -1, //perpetual motion
+    if (animateElement) {
+      const rect = animateElement.getBoundingClientRect();
+
+      // Update particle position based on the clicked element's position
+      setParticlePosition({
+        x: rect.left + rect.width / 2 + "px",
+        y: rect.top + rect.height + "px",
       });
-      setIsOrbiting(true);
     }
+
+    // Show particles for explosion
+    setIsParticleVisible(true);
+
+    // Hide particles after 1 second
+    setTimeout(() => {
+      setIsParticleVisible(false);
+    }, 1000);
+
+    // Animate the clicked image
+    gsap.to(animateElement, {
+      x: "-10", // Move to the far right of the screen
+      y: "-100vh", // Move up
+      scale: 0.2, // Shrink the image to simulate zooming out
+      duration: 2,
+      ease: "power4.inOut",
+    });
   };
 
-  //   const handleClick = () => {
-  //     const animateElement = document.querySelector("#kelli-image");
-
-  //     gsap.to(animateElement, {
-  //       x: "100vw", // Move to the far right of the screen
-  //       y: "-50vh", // Move up
-  //       scale: 0.5, // Shrink the image to simulate zooming out
-  //       rotation: 360, // Rotate as it moves
-  //       duration: 2,
-  //       ease: "power4.inOut",
-  //     //   onComplete: () => {
-  //     //     // Optional: After the animation completes, you could bring the image back
-  //     //     gsap.to(animateElement, { x: "0", y: "0", scale: 1, duration: 1 });
-  //     //   },
-  //     });
-  //   };
   return (
-    <div className="fixed -bottom-[125%] my-9 right-0 z-10 flex -translate-y-1/2 flex-col min-h-[100vh]">
-      <div className="relative h-full w-[200px]">
-        <Image
-          src={kelli2}
-          height={150}
-          alt="kelli"
-          id="kelli-image"
-          className="absolute -top-10 z-10 "
-          onClick={handleOrbitClick} //
-        />
-        <div className="absolute right-5 top-16 flex flex-col gap-2 items-center justify-center hover:scale-110 hover:z-[11] cursor:pointer">
+    <>
+      {isParticleVisible && (
+        // @ts-ignore
+        <ParticleExplosion id="particles" className="w-full h-full" />
+      )}
+      <div className="fixed -bottom-[125%] my-9 right-0 z-10 flex -translate-y-1/2 flex-col min-h-[100vh]">
+        <div className="relative h-full w-[200px]">
           <Image
-            src={message}
-            height={75}
-            width={75}
-            alt="message"
-            className="shadow-customGray rounded-full border-[1px] border-opacity-55 border-customIndigo"
+            src={kelli2}
+            height={150}
+            alt="kelli"
+            id="kelli-image"
+            className="absolute -top-10 z-10"
+            onClick={handleClick}
           />
-          <p className="p-2 rounded-xl bg-lightGrey bg-opacity-70">
-            Got Questions?
-          </p>
+          <div className="absolute right-5 top-16 flex flex-col gap-2 items-center justify-center hover:scale-110 hover:z-[11] cursor:pointer">
+            <Image
+              src={message}
+              height={75}
+              width={75}
+              alt="message"
+              className="shadow-customGray rounded-full border-[1px] border-opacity-55 border-customIndigo"
+            />
+            <p className="p-2 rounded-xl bg-lightGrey bg-opacity-70">
+              Got Questions?
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
