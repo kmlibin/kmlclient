faaadf15cf896e7e1b1052e97045c897eca901e9
Kelli Libin <km.putnam2@gmail.com>
2024-10-22 17:10:35 -0600

    migrated css to tailwind, added overlay to back image

--- src/components/Home/Portfolio.tsx
+++ src/components/Home/Portfolio.tsx
@@ -1,11 +1,27 @@
 "use client";
 import React, { useState } from "react";
 import BreakoutGame from "../Breakout";
+import Image from "next/image";
 
 import "./Portfolio.css";
 
+type CubeProps = {
+  frontImage?: any;
+  backImage?: any;
+  height: string;
+  complete?: boolean;
+  review?: string;
+  link?: string;
+};
 //@ts-ignore
-const Cube = ({ frontImage, backImage, height }) => {
+const Cube = ({
+  frontImage,
+  backImage,
+  height,
+  complete,
+  review,
+  link,
+}: CubeProps) => {
   const [currentSide, setCurrentSide] = useState("show-front");
   const isCube = height === "250px";
 
@@ -20,28 +36,30 @@ const Cube = ({ frontImage, backImage, height }) => {
   if (isCube) {
     // Render the cube template
     return (
-      <div className={`scene`}>
+      <div className="scene w-[100%] h-auto overflow-hidden ">
         <div
-          className={`cube ${currentSide}`}
+          className={`cube ${currentSide} w-[100%] h-[250px] relative transition-transform duration-1000`}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
-          <div
-            className="cube__face cube__face--front cursor-pointer"
-            style={{
-              backgroundImage: `url(${frontImage})`,
-            }}
-          >
-            front
-          </div>
-          <div
-            className="cube__face cube__face--bottom"
-            style={{
-              backgroundImage: `url(${backImage})`,
-            }}
-          >
-            bottom
-          </div>
+          <Image
+            className="cube__face cube__face--front cursor-pointer absolute w-[100%] h-[100%] border-2 border-black text-3xl text-center text-white"
+            src={frontImage}
+            alt="CW"
+          />
+   
+   <div className="cube__face cube__face--bottom cursor-pointer absolute w-[100%] h-[100%]">
+        <Image
+          className="absolute w-full h-full"
+          src={backImage}
+          alt="Back Image"
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-transparent z-10"></div>
+        <div className="absolute inset-0 z-20 flex items-center justify-center text-white text-xl">
+          Add review text, stars, links
+        </div>
+        </div>
+
         </div>
       </div>
     );
