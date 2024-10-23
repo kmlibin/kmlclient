abf768571547f5dfbdfe220b79e093627056ac58
Kelli Libin <km.putnam2@gmail.com>
2024-10-23 16:10:12 -0600

    added breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "822px",
+      lg: "1023px",
+      xl: "1281px",
+      "2xl": "1800px",
+    },
     extend: {
       colors: {
         brightOrange: "#e15c3a",
