e7217913c6542e529950549d43e7f771d11f6af5
Kelli Libin <km.putnam2@gmail.com>
2024-10-24 16:08:00 -0600

    render home banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import ContactWrapper from "@/components/Wrapper/ContactWrapper";
 import Hero from "@/components/Home/Hero";
 
 import PortfolioGallery from "@/components/Home/PortfolioGallery";
+import HomeBanner from "@/components/Home/HomeBanner";
 
 function Home() {
   return (
     <div className="flex flex-col w-full h-full">
       <Hero />
-      
-        <PortfolioGallery />
-    
+      <HomeBanner />
+      <PortfolioGallery />
     </div>
   );
 }
