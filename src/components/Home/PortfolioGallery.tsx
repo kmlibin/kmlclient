c80dbc8d5f3e77945cb1024059d5247d93029ed7
Kelli Libin <km.putnam2@gmail.com>
2024-10-24 13:01:23 -0600

    working on styling the non-cube elements of the portfolio section. added image heading, bg, and working on subtext

--- src/components/Home/PortfolioGallery.tsx
+++ src/components/Home/PortfolioGallery.tsx
@@ -10,7 +10,10 @@ import fittees2 from "../../../public/images/fittees2.png";
 import cw2 from "../../../public/images/cw2.png";
 import kellinetlify2 from "../../../public/images/kellinetlify2.png";
 import jpel2 from "../../../public/images/jpel2.png";
+import myport from "../../../public/images/7.png";
 
+import portfolio from "../../../public/images/portfolio.svg";
+import Image from "next/image";
 const PortfolioGallery = () => {
   const portfolioItems = [
     {
@@ -38,26 +41,20 @@ const PortfolioGallery = () => {
         "This is a personal site that I use for larger employers, as the projects are more skill focused and not nearly as pretty :)",
       backImage: kellinetlify2,
     },
+
+    {
+      id: 7,
+      height: "400px",
+    },
     {
       id: 4,
       height: "175px",
       bubbles: true,
       review: "this is a review",
     },
-
-    {
-      id: 7,
-      height: "400px",
-    },
   ];
 
   const portfolioItems2 = [
-    {
-      id: 4,
-      height: "175px",
-      bubbles: true,
-      review: "this is a review x 2",
-    },
     {
       id: 1,
       height: "400px",
@@ -76,45 +73,56 @@ const PortfolioGallery = () => {
       backImage: jpel2,
     },
     {
+      id: 4,
+      height: "175px",
+      bubbles: true,
+      review: "this is a review x 2",
+    },
+    {
       id: 8,
       height: "466px",
     },
   ];
 
   return (
-    <>
-
-    <div className="grid grid-cols-2 justify-start items-start gap-4 px-5 2xl:w-2/3 py-32 w-4/5 mx-auto">
-      <div className="flex flex-col justify-start gap-4">
-        {portfolioItems.map((item) => (
-          <PortfolioCard
-            key={item.id}
-            height={item.height}
-            complete={item.complete}
-            review={item.review}
-            link={item.link}
-            bubbles={item.bubbles}
-            frontImage={item.frontImage}
-            backImage={item.backImage}
-          />
-        ))}
+    <div className=" w-full relative bg-customWhite justify-start items-start flex  pt-2">
+      <div className="absolute w-2/3 bg-customGoldRGBA h-[100%] left-2 border-2 border-customGold"></div>
+      <div className="absolute w-[95%] bg-customPink h-[10%] left-[18.5%] top-[3%] border-2 border-customPink z-10"></div>
+      <div className="sticky top-[14rem]">
+        <Image alt="portfolio" src={myport} className="w-[350px]" />
       </div>
-      <div className="flex flex-col justify-start gap-4 ">
-        {portfolioItems2.map((item) => (
-          <PortfolioCard
-            key={item.id}
-            link={item.link}
-            height={item.height}
-            bubbles={item.bubbles}
-            complete={item.complete}
-            review={item.review}
-            frontImage={item.frontImage}
-            backImage={item.backImage}
-          />
-        ))}
+
+      <div className="h-full grid grid-cols-2 justify-center items-start gap-4 p-4 w-4/5 mr-6 3xl:w-2/3 mt-[220px] box-content bg-lightGrey z-20">
+        <div className="flex flex-col justify-start gap-4 w-full">
+          {portfolioItems.map((item) => (
+            <PortfolioCard
+              key={item.id}
+              height={item.height}
+              complete={item.complete}
+              review={item.review}
+              link={item.link}
+              bubbles={item.bubbles}
+              frontImage={item.frontImage}
+              backImage={item.backImage}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col justify-start gap-4 ">
+          {portfolioItems2.map((item) => (
+            <PortfolioCard
+              key={item.id}
+              link={item.link}
+              height={item.height}
+              bubbles={item.bubbles}
+              complete={item.complete}
+              review={item.review}
+              frontImage={item.frontImage}
+              backImage={item.backImage}
+            />
+          ))}
+        </div>
       </div>
     </div>
-    </>
   );
 };
 
