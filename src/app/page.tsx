1dfe483044f93572a2f017c2d54efecac14f56f9
Kelli Libin <km.putnam2@gmail.com>
2024-10-28 16:16:11 -0600

    move components around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,19 @@ import ContactWrapper from "@/components/Wrapper/ContactWrapper";
 import Hero from "@/components/Home/Hero";
 
 import PortfolioGallery from "@/components/Home/PortfolioGallery";
+import PortfolioBanner from "@/components/Home/PortfolioBanner";
 import HomeBanner from "@/components/Home/HomeBanner";
+import Info from "@/components/Home/Info";
 
 function Home() {
   return (
     <div className="flex flex-col w-full h-full">
       <Hero />
-      <HomeBanner />
-      <PortfolioGallery />
+      <HomeBanner /> 
+      <PortfolioBanner />
+      <Info />
+      
+     
     </div>
   );
 }
