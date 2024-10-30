29242af9a224e37644c8ff039333ac853ce821ed
Kelli Libin <km.putnam2@gmail.com>
2024-10-30 13:25:48 -0600

    adjusting mass and adding style to the a.div

--- src/components/Home/Trail.tsx
+++ src/components/Home/Trail.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const Trail: React.FC<{ open: boolean; children: React.ReactNode }> = ({ open, children }) => {
     const items = React.Children.toArray(children)
     const trail = useTrail(items.length, {
-      config: { mass: 15, tension: 2000, friction: 200 },
+      config: { mass: 12, tension: 2000, friction: 200 },
       opacity: open ? 1 : 0,
       x: open ? 0 : 20,
       height: open ? 50 : 0,
@@ -17,8 +17,8 @@ const Trail: React.FC<{ open: boolean; children: React.ReactNode }> = ({ open, c
     return (
       <div>
         {trail.map(({ height, ...style }, index) => (
-          <a.div key={index} className="trailsText">
-            <a.div style={{ height }}>{items[index]}</a.div>
+          <a.div key={index} className="trailsText" style={style}>
+            <a.div style={{ height }}>{items[index]} </a.div>
           </a.div>
         ))}
       </div>
