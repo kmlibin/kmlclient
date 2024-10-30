c3a14fd7d069aeb69fedb14f338b94ffaf4e29e6
Kelli Libin <km.putnam2@gmail.com>
2024-10-30 12:13:15 -0600

    adding logos for tab and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ibm } from "@/app/utils/fonts";
-import webdevlogo from "../../public/images/colorpng.png";
+import webdevlogo from "../../public/images/fulllogo.png";
 import { RiCopyrightLine } from "react-icons/ri";
 import Image from "next/image";
 
@@ -11,9 +11,9 @@ const Footer = () => {
     <div
       className={`${ibm.className} w-full h-[150px] bg-customIndigoRGBA flex flex-col border-t-customIndigo border-t`}
     >
-      <div className="relative w-full border-b-4 h-[120px] border-b-customIndigo">
+      <div className="relative w-full border-b-4 h-[100px] border-b-customIndigo">
         <Image
-          className="absolute -bottom-5 left-20 z-[10] h-[120px] w-[120px] border-none"
+          className="absolute -bottom-[3rem] left-20 z-[10] h-[130px] w-auto border-none"
           src={webdevlogo}
           alt="libin web development logo"
         />
@@ -22,7 +22,13 @@ const Footer = () => {
         Libin Web Development <RiCopyrightLine /> 2024
       </p>
     </div>
+
+    
   );
 };
+
+
+
+
 
 export default Footer;
