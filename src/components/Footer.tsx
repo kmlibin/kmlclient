39dc341d56b8f354ab950e416cff0a2f318e4a88
Kelli Libin <km.putnam2@gmail.com>
2024-10-30 14:30:22 -0600

    adding margin to copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ type Props = {};
 const Footer = () => {
   return (
     <div
-      className={`${ibm.className} w-full h-[150px] bg-customIndigoRGBA flex flex-col border-t-customIndigo border-t`}
+      className={`${ibm.className} w-full h-[150px] bg-customIndigoRGBA flex flex-col  border-t-customIndigo border-t`}
     >
       <div className="relative w-full border-b-4 h-[100px] border-b-customIndigo">
         <Image
@@ -18,7 +18,7 @@ const Footer = () => {
           alt="libin web development logo"
         />
       </div>
-      <p className="w-full items-center text-md gap-4  pr-5 flex justify-end">
+      <p className="w-full items-center text-md gap-4  pr-5 flex justify-end mt-3">
         Libin Web Development <RiCopyrightLine /> 2024
       </p>
     </div>
