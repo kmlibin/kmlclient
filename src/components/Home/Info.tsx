006a29bafae9af43ef0b5f6cad1b85641e3f28a9
Kelli Libin <km.putnam2@gmail.com>
2024-11-01 16:40:58 -0600

    display the header

--- src/components/Home/Info.tsx
+++ src/components/Home/Info.tsx
@@ -12,13 +12,14 @@ const Info = () => {
       id="info-section"
       className="relative flex w-full flex-col justify-center items-center gap-2 bg-customWhite pb-[6rem]"
     >
-      {/* <div className="mb-[5rem]">
+      
+      <div className="mb-[5rem]">
       <Zoom direction="down" duration={1500} triggerOnce>
         <h2 className={`${fredoka.className} text-5xl`}>
           Don’t Settle for Less — Get a Site That Works
         </h2>
         </Zoom>
-      </div> */}
+      </div>
       <div className="flex w-5/6 justify-evenly items-start gap-14 h-full ">
         {infoCards.map((card, index) => (
           <InfoCard
