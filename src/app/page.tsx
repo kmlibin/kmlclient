2e0aad39d4db2fdb69da60708ee6b04ac38edb92
Kelli Libin <km.putnam2@gmail.com>
2024-11-06 14:27:12 -0700

    wrapped in home contact wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-import ContactWrapper from "@/components/Wrapper/ContactWrapper";
-import Hero from "@/components/Home/Hero";
-import PortfolioBanner from "@/components/Home/Portfolio2Banner";
-import HomeBanner from "@/components/Home/HomeBanner";
-import Info from "@/components/Home/Info";
+import HomeContactWrapper from "@/components/Wrapper/HomeContactWrapper";
+import Hero from "@/components/Home/Hero/Hero";
+import PortfolioBanner from "@/components/Home/PortfolioBanner/Portfolio2Banner";
+import HomeBanner from "@/components/Home/Hero/HomeBanner";
+import Info from "@/components/Home/InfoBanner/Info";
 
 function Home() {
   return (
     <div className="flex flex-col w-full h-full">
       <Hero />
       <HomeBanner />
-
       <Info />
       <PortfolioBanner />
     </div>
   );
 }
 
-export default ContactWrapper(Home);
+export default HomeContactWrapper(Home);
