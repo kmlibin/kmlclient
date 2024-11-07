7a204422bd9d315edc70f4fd98e05dd00c9e3651
Kelli Libin <km.putnam2@gmail.com>
2024-11-07 10:44:30 -0700

    made alts, put emailjs one in JSX to accommodate links and hover effects

--- src/app/about/pricing/pricingContent.js
+++ src/app/about/pricing/pricingContent.js
@@ -5,47 +5,59 @@ import multiple from "../../../../public/images/multiple.svg";
 import message from "../../../../public/images/message.svg";
 import seo from "../../../../public/images/seo.svg";
 import hosting from "../../../../public/images/hosting.svg";
-import { fredoka, ibm } from "@/app/utils/fonts";
+import { fredoka, ibm, ibmBold } from "@/app/utils/fonts";
+import Link from "next/link";
 
 export const pricingCards = [
   {
     title: "Custom Next.js Website",
-    alt: "design",
+    alt: "a computer logo with a paintbrush and image",
     src: design,
     text: "A unique, fully custom-designed website crafted to reflect your brand’s identity. Built with performance and scalability in mind, your site will be fast, SEO-friendly, and seamlessly adapt to any device, ensuring an exceptional experience for your visitors across all screen sizes.",
     isList: false,
   },
   {
     title: "Direct Communication & Personalized Support",
-    alt: "responsive design",
+    alt: "icon of two people shaking hands",
     src: communication,
     text: "You’ll work directly with me throughout the process, ensuring your site reflects your vision. I’ll be available for ongoing support to address any questions or minor adjustments.",
     isList: false,
   },
   {
     title: "Responsive, Mobile-Optimized Design",
-    alt: "responsive design",
+    alt: "icon of a laptop, tablet, and phone",
     src: responsive,
     text: "Your website will be fully responsive, ensuring it looks and functions seamlessly on mobile, tablet, and desktop devices.",
     isList: false,
   },
   {
     title: "Up to Four Pages",
-    alt: "responsive design",
+    alt: "logo of four pages on top of each other",
     src: multiple,
     text: "Your website includes up to four pages, covering the essentials for a professional online presence. Need more pages? No problem—each additional page can be added for just $50.",
     isList: false,
   },
   {
-    title: "Contact Form Integration",
-    alt: "responsive design",
+    title: (
+      <p className={`${fredoka.className} text-2xl`}>
+        Contact Form Integration
+      </p>
+    ),
+
+    alt: "logo of a message bubble",
     src: message,
-    text: "Set up a custom contact form for client inquiries and engagement. Note: Clients will need an EmailJS account (using the free tier). See FAQ for details.",
-    isList: false,
+    text: (
+      <p className={`${ibm.className} text-md`}>
+        Set up a custom contact form for client inquiries and engagement.<br></br> <small><span className={`${ibmBold.className} underline`}>Note:</span>
+        &nbsp; Clients will need an EmailJS account (using the free tier). <Link href={"/about/faq"} className="cursor:hover text-customBlue hover:underline hover:text-customIndigo duration-300 transition-all">See FAQ for
+        details.</Link></small>
+      </p>
+    ),
+    isList: true,
   },
   {
     title: "Optimized for Speed & SEO",
-    alt: "responsive design",
+    alt: "logo of magnifying class with SEO in the middle",
     src: seo,
     text: "Your website will be built with Next.js, a framework known for its exceptional speed and performance—key factors that search engines love. With built-in SEO features like server-side rendering (SSR), a custom robots.txt, and an auto-generated sitemap, your site will be fully optimized for faster indexing and better visibility in search results.",
     isList: false,
@@ -53,10 +65,13 @@ export const pricingCards = [
   {
     title: (
       <p className={`${fredoka.className} text-2xl w-full`}>
-        Reliable Hosting & Content Updates <span className={`${ibm.className} text-lg`}> &nbsp;- $60 per year</span>
+        Reliable Hosting & Content Updates<br></br>
+        <span className={`${ibmBold.className} text-lg`}>
+          - additional $60 per year
+        </span>
       </p>
     ),
-    alt: "hosting",
+    alt: "logo of servers",
     src: hosting,
     text: (
       <>
