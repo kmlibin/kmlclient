b8546c65a190c6640d645ef58a3521d311424ec3
Kelli Libin <km.putnam2@gmail.com>
2024-11-08 14:48:18 -0700

    adjusting styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <div className="relative w-full">
       <div
-        className={`absolute top-2 left-0 h-[100px] w-2/3 bg-customIndigo shadow-customSmallGrey text-customWhite p-4 rounded-tr-lg rounded-br-lg
+        className={`absolute top-2 left-0 h-[100px] w-2/3 bg-customIndigo  text-customWhite p-4 rounded-tr-lg rounded-br-lg
               transform ${isOpen ? "translate-x-0" : "-translate-x-full"}
               transition-transform duration-300 ease-in-out z-40 flex items-center`}
       >
