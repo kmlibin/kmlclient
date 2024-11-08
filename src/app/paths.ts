caea5fb5d750614d66b5c9cf39fab9c1e4165170
Kelli Libin <km.putnam2@gmail.com>
2024-11-08 11:58:14 -0700

    create paths file

---
+++ src/app/paths.ts
@@ -0,0 +1,22 @@
+const paths = {
+  homePath() {
+    return "/";
+  },
+  faqPath() {
+    return "about/faq";
+  },
+  aboutPath() {
+    return "/about";
+  },
+  pricingPath() {
+    return "/about/pricing";
+  },
+  contactPath() {
+    return "/contact";
+  },
+  portfolioPath() {
+    return "/portfolio";
+  },
+};
+
+export default paths;
