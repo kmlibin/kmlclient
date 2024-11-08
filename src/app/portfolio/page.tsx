f2006e038797628e0b32330b74f644acf53a9b98
Kelli Libin <km.putnam2@gmail.com>
2024-11-08 10:45:07 -0700

    create portfolio page and render gallery

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar";
+import PortfolioGallery from "./PortfolioGallery";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <Navbar />
+      <PortfolioGallery />
+    </>
+  );
+};
+
+export default page;
