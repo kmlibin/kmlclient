1b48fb5822a5ad6ef33026a13a343c820ba6f358
Kelli Libin <km.putnam2@gmail.com>
2024-11-11 16:45:04 -0700

    higher z-index for the rainbow

--- src/app/portfolio/PortfolioPage.tsx
+++ src/app/portfolio/PortfolioPage.tsx
@@ -103,10 +103,10 @@ const PortfolioPage = () => {
     <div className="flex  w-full bg-customWhite">
       <div className=" relative justify-start items-start flex mb-10">
         {/* rainbows */}
-        <div className="absolute -top-[11rem] -left-[15rem] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px]"></div>
+        <div className="absolute -top-[11rem] -left-[15rem] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px] z-[25]"></div>
         <div className="absolute -bottom-[11rem] -right-[15rem] rainbow-circle w-[500px] h-[500px] rounded-full "></div>
 
-        <div className="sticky top-[168px]">
+        <div className="sticky top-[168px] z-[35]">
           <Image alt="portfolio" src={myport} className="w-[450px]" />
         </div>
 
