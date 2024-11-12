7e73e261346448d2d578eb7a8aa83ccd48ccb16a
Kelli Libin <km.putnam2@gmail.com>
2024-11-12 13:47:19 -0700

    create and display fireworks for success modal

---
+++ src/app/contact/Fireworks.tsx
@@ -0,0 +1,177 @@
+"use client";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
+
+// Libraries
+
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+
+// Types
+import { Container, ISourceOptions, Engine } from "@tsparticles/engine";
+import { tsParticles } from "tsparticles-engine";
+
+type FireworksProps = {
+  id: string;
+};
+
+const Fireworks = ({ id }: FireworksProps) => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+      //@ts-ignore
+      await loadFireworksPreset(tsParticles);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+  console.log(init);
+
+  const particlesConfig: ISourceOptions = useMemo(
+    () => ({
+      preset: "fireworks",
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      background: {
+        color: {
+          value: "rgba(0,0,0, .5)",
+        },
+      },
+      emitters: {
+        life: {
+          count: 0,
+          duration: 20,
+          delay: 0.3,
+        },
+        rate: {
+          delay: 0.08,
+          quantity: 1,
+        },
+        size: {
+          width: 100,
+          height: 0,
+        },
+        position: {
+          y: 100,
+          x: 50,
+        },
+      },
+      particles: {
+        // properties of the main firework particle
+        number: {
+          value: 0, // Randomize the number of particles
+        },
+        destroy: {
+          mode: "split",
+          split: {
+            count: 1,
+            factor: { value: 1 / 3 },
+            rate: {
+              value: 100,
+            },
+            particles: {
+              color: {
+                value: ["#FF0000", "#FFD700", "#00FF00", "#00BFFF", "#FF1493"],
+              },
+              stroke: {
+                width: 0,
+              },
+              number: {
+                value: 0,
+              },
+              collisions: {
+                enable: false,
+              },
+              opacity: {
+                value: 1,
+                animation: {
+                  enable: true,
+                  speed: 0.6,
+                  minimumValue: 0.1,
+                  sync: false,
+                  startValue: "max",
+                  destroy: "min",
+                },
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 2, max: 3 },
+                animation: {
+                  enable: false,
+                },
+              },
+            },
+          },
+        },
+        life: {
+          count: 10,
+          duration: 20,
+        },
+        shape: {
+          type: "line", // Linear path for the initial explosion
+        },
+        size: {
+          value: { min: 1, max: 100 },
+          animation: {
+            enable: true,
+            sync: true,
+            speed: 50,
+            destroy: "min",
+          },
+        },
+        stroke: {
+          color: {
+            value: "#303030",
+          },
+          width: 3,
+        },
+        rotate: {
+          path: true, // Single path for smoother particle rotation
+        },
+        move: {
+          enable: true,
+          gravity: {
+            acceleration: 5, // Stronger gravity acceleration for downward movement
+            enable: true,
+            inverse: true, // Keep the particles on the same path for fireworks effect
+            maxSpeed: 20,
+          },
+          speed: { min: 5, max: 10 }, // Faster particle movement for a more explosive effect
+          outModes: {
+            default: "destroy",
+            top: "none",
+          },
+          trail: {
+            fillColor: "#000",
+            length: 20,
+          },
+        },
+      },
+    }),
+    []
+  );
+
+
+  if (init) {
+    return (
+      <Particles
+        id={id}
+        particlesLoaded={particlesLoaded}
+        options={particlesConfig}
+      />
+    );
+  }
+
+  return null;
+};
+
+export default Fireworks;
--- src/app/contact/SuccessErrorModal.tsx
+++ src/app/contact/SuccessErrorModal.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { IoMdClose } from "react-icons/io";
 import { fredoka, ibm } from "../utils/fonts";
 import { useRouter } from "next/navigation";
-
+import Fireworks from "./Fireworks";
 type SuccessOrErrorModalProps = {
   setIsOpen: (isOpen: boolean) => void;
   submissionStatus: { heading: string | null; message: string | null } | null;
@@ -23,7 +23,7 @@ const SuccessErrorModal = ({
 }: SuccessOrErrorModalProps) => {
   const router = useRouter();
   const handleClose = () => {
-    if (submissionStatus && submissionStatus.heading === "Success") {
+    if (submissionStatus && submissionStatus.heading === "You're all set!") {
       router.push("/");
       setIsOpen(false);
       setSubmissionStatus(null);
@@ -34,57 +34,53 @@ const SuccessErrorModal = ({
   };
   return (
     <>
-    <div
-      className="fixed inset-0 z-50 bg-black bg-opacity-50"
-      onClick={() => setIsOpen(false)}
-    />
-
-    {/* modal start */}
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center"
-
-    >
-      <div className="relative w-full max-w-sm transform rounded-xl bg-customWhite p-6 shadow-xl">
-        <div className="mb-4 flex items-center justify-between">
-          <h5
-            className={`${fredoka.className} w-full border-b-2 border-lighterTeal pb-4 text-center text-lg font-semibold text-darkestTeal`}
-          >
-            {submissionStatus?.heading}
-          </h5>
-          <button
-            className="text-gray-500 hover:text-darkestTeal"
-            onClick={() => {
-              setIsOpen(false);
-              setSubmissionStatus(null);
-            }}
-          >
-            <IoMdClose
-              size={24}
-              className="absolute right-4 top-4 text-xl text-gray-500 hover:text-darkestTeal"
-            />
-          </button>
+      {submissionStatus?.heading == "You're all set!" ? (
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-50">
+          <Fireworks id="tsparticles" />
         </div>
+      ) : (
+        <div className="fixed min-w-full min-h-full z-50 bg-black bg-opacity-60"></div>
+      )}
+      {/* modal start */}
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="relative w-full max-w-sm transform rounded-xl bg-customWhite p-6 shadow-xl">
+          <div className="mb-4 flex items-center justify-between">
+            <h5
+              className={`${fredoka.className} w-full border-b-2 border-lighterTeal pb-4 text-center text-lg font-semibold text-darkestTeal`}
+            >
+              {submissionStatus?.heading}
+            </h5>
+            <button
+              className="text-gray-500 hover:text-darkestTeal"
+              onClick={() => {
+                setIsOpen(false);
+                setSubmissionStatus(null);
+              }}
+            >
+              <IoMdClose
+                size={24}
+                className="absolute right-4 top-4 text-xl text-gray-500 hover:text-darkestTeal"
+              />
+            </button>
+          </div>
 
-        {/* text */}
-        <div
-          className={`${ibm.className} mb-6 text-center text-darkestTeal`}
-        >
-          <p>{submissionStatus?.message}</p>
-        </div>
+          {/* text */}
+          <div className={`${ibm.className} mb-6 text-center text-darkestTeal`}>
+            <p>{submissionStatus?.message}</p>
+          </div>
 
-        <div className="flex justify-center">
-          <button
-            className={`${fredoka.className} mt-4 w-full border-2 border-lighterTeal bg-darkTeal py-3 tracking-wider text-lightGrey shadow-md transition-all duration-300 hover:bg-customGold active:translate-y-[2px] active:scale-[0.95] active:shadow-none`}
-            onClick={handleClose}
-          >
-            Close
-          </button>
+          <div className="flex justify-center">
+            <button
+              className={`${fredoka.className} mt-4 w-full border-2 border-lighterTeal bg-darkTeal py-3 tracking-wider text-lightGrey shadow-md transition-all duration-300 hover:bg-customGold active:translate-y-[2px] active:scale-[0.95] active:shadow-none`}
+              onClick={handleClose}
+            >
+              Close
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-
-  )
+    </>
+  );
 };
 
 export default SuccessErrorModal;
