07bbf414e54817010d6df37a98eee22d5e463f59
Kelli Libin <km.putnam2@gmail.com>
2024-11-12 16:35:59 -0700

    adjusting padding and bg for dropdown

--- src/app/about/faq/Accordion.tsx
+++ src/app/about/faq/Accordion.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ReactNode } from "react";
 import { fredoka } from "@/app/utils/fonts";
 import { FaPlus, FaMinus } from "react-icons/fa";
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
@@ -9,7 +9,7 @@ type Props = {
   item: {
     id: string;
     question: string;
-    answer: string;
+    answer: string | ReactNode;
   };
   openId: string | null;
 };
@@ -18,16 +18,16 @@ const Accordion = ({ toggleItem, item, openId }: Props) => {
   return (
     <div
       id={item.id}
-      className="border-b border-gray-300 w-full py-4 flex flex-col items-start"
+      className="border-b border-gray-300 w-full py-4 flex flex-col items-start gap-2 "
     >
       <button
         onClick={() => toggleItem(item.id)}
         className="w-full flex items-center justify-between px-4 py-2"
       >
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6">
           <HiOutlineComputerDesktop className="text-customPink" />
           {/* <FaQuestion className="text-customPink" /> */}
-          <span className={`${fredoka.className} text-2xl`}>
+          <span className={`${fredoka.className} text-2xl flex  text-left`}>
             {item.question}
           </span>
         </div>
@@ -39,13 +39,14 @@ const Accordion = ({ toggleItem, item, openId }: Props) => {
           )}
         </div>
       </button>
+      
       <div
         className={`transition-all duration-300 overflow-hidden w-full ${
-          openId === item.id ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+          openId === item.id ? " opacity-100" : "max-h-0 opacity-0"
         }`}
         style={{ transitionProperty: "max-height, opacity" }}
       >
-        <div className="px-8 py-4 bg-customIndigo bg-opacity-10 w-4/5 ml-[3rem] text-[1rem] flex justify-start">
+        <div className="px-8 py-4 bg-lightIndigo w-4/5 ml-[3rem] text-[1rem] flex justify-start items-start">
           <p>{item.answer}</p>
         </div>
       </div>
