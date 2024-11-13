4c31d5ac098eb5cccb92692a719a91554188db88
Kelli Libin <km.putnam2@gmail.com>
2024-11-13 16:53:52 -0700

    new layout for non-home, that way navbar can be rendered

---
+++ src/app/main/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function NotHomeLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <div className={`flex w-full flex-col bg-customWhite justify-center items-center`}>
+        <Navbar />
+        {children}
+      </div>
+    );
+  }
