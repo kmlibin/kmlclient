fc95cf44037f309d46f7e8de3a80629bd8cd6a50
Kelli Libin <km.putnam2@gmail.com>
2024-11-14 12:57:51 -0700

    adding priority to images

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -21,10 +21,10 @@ const Hero = () => {
       <div className="absolute inset-0 -z-10">
         <Image
           src={background}
-          priority
           fill
           style={{ objectFit: "cover" }}
           alt="rainbow background"
+          priority
         />
         <div className="absolute inset-0 w-1/3 bg-gradient-to-r from-gray-900 to-transparent"></div>
       </div>
@@ -32,7 +32,12 @@ const Hero = () => {
 
       <div className="relative flex justify-end items-center ml-[5rem] py-10 mb-4">
         <Fade triggerOnce>
-          <Image src={bwsvg4} width={650} alt="Libin Web Development and image of Kelli" priority />
+          <Image
+            src={bwsvg4}
+            width={650}
+            alt="Libin Web Development and image of Kelli"
+            priority
+          />
         </Fade>
         <div className="absolute -right-[52%] bottom-0 flex flex-col justify-end mb-2 py-10 text-gray-900 text-2xl leading-loose list-none">
           <NavLinks />
