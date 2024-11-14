9a1c5909465bebf8ef179634533a1e1f14c4e222
Kelli Libin <km.putnam2@gmail.com>
2024-11-14 13:00:48 -0700

    added key

--- src/app/main/about/pricing/AdditionalInfo.tsx
+++ src/app/main/about/pricing/AdditionalInfo.tsx
@@ -27,7 +27,7 @@ const AdditionalInfo = () => {
         >
           {additionalInfo.map((item: listItem, index) =>
             item.isNote2 ? (
-              <p>
+              <p key={index}>
                 <span className={`text-lg ${ibmBold.className} underline`}>
                   Note on Self-Hosting:
                 </span>
