3707aa84a266b2b38ba3d5e237f6ce7106674769
Kelli Libin <km.putnam2@gmail.com>
2024-11-15 12:04:45 -0700

    rainbow is at bottom of this one, myb

--- src/components/Home/PortfolioBanner/Portfolio2Banner.tsx
+++ src/components/Home/PortfolioBanner/Portfolio2Banner.tsx
@@ -1,9 +1,9 @@
-
 import React from "react";
 //fonts and utils
 import { fredoka, ibm } from "@/app/utils/fonts";
 import paths from "@/app/paths";
 import { portfolioBanner } from "../homeContent";
+
 //libraries
 import { FcIdea } from "react-icons/fc";
 //components
@@ -15,16 +15,17 @@ const Portfolio2Banner = () => {
   return (
     <div
       id="portfolio-section"
-      className="relative bg-gradient-to-t from-customIndigoRGBA to-customWhite flex flex-col justify-center items-center w-full pb-[4rem]"
+      className="relative flex flex-col justify-center items-center w-full pb-[4rem] bg-customWhite"
     >
+      <div className="absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px] -z-1"></div>
       <ZoomHeading
         as="h3"
-        className={`${fredoka.className} text-5xl w-full tracking-wide flex items-center justify-center gap-3`}
+        className={`${fredoka.className} text-5xl w-full tracking-wide flex items-center justify-center gap-3 pt-10  z-10`}
       >
         Bringing Ideas to Life <FcIdea />
       </ZoomHeading>
 
-      <div className="w-2/3 text-md  bg-customWhite bg-opacity-55 shadow-indigo rounded-xl p-5 my-10">
+      <div className="w-2/3 text-[16px] bg-customWhite bg-opacity-55 shadow-indigo rounded-xl p-5 my-10">
         <p className={`${ibm.className} text-center px-5`}>
           {portfolioBanner.mainText}
         </p>
