c3208be34ad695cf8945eaaebbcea9a06fb45014
Kelli Libin <km.putnam2@gmail.com>
2024-11-15 12:25:27 -0700

    no blurb, just header

--- src/components/Home/PortfolioBanner/Portfolio2Banner.tsx
+++ src/components/Home/PortfolioBanner/Portfolio2Banner.tsx
@@ -15,21 +15,21 @@ const Portfolio2Banner = () => {
   return (
     <div
       id="portfolio-section"
-      className="relative flex flex-col justify-center items-center w-full pb-[4rem] bg-customWhite"
+      className="relative flex flex-col justify-center items-center w-full pb-[4rem]"
     >
       <div className="absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px] -z-1"></div>
       <ZoomHeading
         as="h3"
-        className={`${fredoka.className} text-5xl w-full tracking-wide flex items-center justify-center gap-3 pt-10  z-10`}
+        className={`${fredoka.className} text-5xl w-full tracking-wide flex items-center justify-center gap-3 pt-10  mb-10 z-10`}
       >
         Bringing Ideas to Life <FcIdea />
       </ZoomHeading>
 
-      <div className="w-2/3 text-[16px] bg-customWhite bg-opacity-55 shadow-indigo rounded-xl p-5 my-10">
+      {/* <div className="w-2/3 text-[16px] bg-customWhite bg-opacity-55 shadow-indigo rounded-xl p-5 my-10">
         <p className={`${ibm.className} text-center px-5`}>
           {portfolioBanner.mainText}
         </p>
-      </div>
+      </div> */}
       <Slider />
       <div className="flex justify-center mt-[3rem] items-center w-5/6">
         <Button path={paths.portfolioPath()} text="See More" />
