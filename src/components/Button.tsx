30037e7451943c17c2ab8c71131f89098bb85f62
Kelli Libin <km.putnam2@gmail.com>
2024-11-15 12:05:23 -0700

    added shadow

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({path, text}: Props) => {
   return (
     <Link
     href={path}
-    className={` ${ibm.className}  bg-customIndigo relative overflow-hidden z-10 btn p-4 text-lightGrey flex items-center gap-3 justify-center`}
+    className={` ${ibm.className}  bg-customIndigo shadow-customSmallGrey relative overflow-hidden z-10 btn p-4 text-lightGrey flex items-center gap-3 justify-center`}
   >
     <span className="flex justify-center items-center gap-3">{text} <PiArrowFatLinesRightFill /></span>
   </Link>
