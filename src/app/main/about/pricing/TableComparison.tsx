32788791a395a99a181bf73e54cb0d766e9407a0
Kelli Libin <km.putnam2@gmail.com>
2024-12-04 15:07:00 -0700

    responsive, but hide on small screens

--- src/app/main/about/pricing/TableComparison.tsx
+++ src/app/main/about/pricing/TableComparison.tsx
@@ -9,17 +9,17 @@ import table from './images/table.svg'
 
 const TableComparison = () => {
   return (
-    <div id="comparison-table" className="w-[95%] flex flex-col tracking-wide items-center justify-center border-2 border-customBlue mb-10 border-opacity-50">
-      <h3 className={`${fredoka.className} text-4xl mb-4 pt-8 `}>
+    <div id="comparison-table" className="hidden sm:flex w-[95%] flex flex-col tracking-wide items-center justify-center border-2 border-customBlue mb-10 border-opacity-50">
+      <h3 className={`${fredoka.className} text-center text-4xl mb-4 pt-8 `}>
         See How We Compare to Other Platforms
       </h3>
-      <p className={`${ibm.className} text-lg w-3/4 text-center`}>
+      <p className={`${ibm.className} w-[90%] text-lg sm:w-3/4 text-center`}>
         Our flat-fee approach includes everything you need to establish a
         strong, professional online presence — without the surprise charges.
         Compare below to see the comprehensive features of our package against
         common options in the market.
       </p>
-      <Image src={table} alt="table" className="-mt-8" />
+      <Image src={table} alt="table" className="sm:-mt-8" />
     </div>
   );
 };
