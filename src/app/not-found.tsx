652027bc3ff057edfdd9c1528bbd3a8c61e148c3
Kelli Libin <km.putnam2@gmail.com>
2024-12-05 16:34:37 -0700

    delete unused props, esc out apostrophe

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,16 +7,15 @@ import Link from "next/link";
 import paths from "./paths";
 import { FaHouse } from "react-icons/fa6";
 
-type Props = {};
 
-const notFound = (props: Props) => {
+const notFound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-[80vh] w-full text-blackTextFont">
       <div className="h-full w-full flex justify-center items-center flex-col">
         <h2 className={`${fredoka.className} text-5xl py-4 tracking-wide`}>
-          Uh-Oh! This Page Didn't Make It...
+          Uh-Oh! This Page Didn&apos;t Make It...
         </h2>
-        <p className={`${ibm.className} text-xl my-4 flex`}>No worries -head to the <Link className={`${ibmBold.className} hover:text-customBlue duration-300`} href={paths.homePath()}><span className="flex items-center justify-center gap-2">&nbsp;<FaHouse /> homepage&nbsp;</span></Link> and start fresh!</p>
+        <p className={`${ibm.className} text-xl my-4 flex`}>No worries - head to the <Link className={`${ibmBold.className} hover:text-customBlue duration-300`} href={paths.homePath()}><span className="flex items-center justify-center gap-2">&nbsp;<FaHouse /> homepage&nbsp;</span></Link> and start fresh!</p>
         <Image src={notfound} alt="page not found" />
         
       </div>
