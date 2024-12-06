b1973030a5f1ac5e545f3aff80c987c0b74b62a8
Kelli Libin <km.putnam2@gmail.com>
2024-12-06 14:28:21 -0700

    format

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,14 +1,16 @@
 import Navbar from "@/components/Navbar/Navbar";
 
 export default function NotHomeLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <div className={`flex w-full flex-col bg-customWhite justify-center items-center !overflow-hidden`}>
-        <Navbar />
-        {children}
-      </div>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={`flex w-full flex-col bg-customWhite justify-center items-center`}
+    >
+      <Navbar />
+      {children}
+    </div>
+  );
+}
