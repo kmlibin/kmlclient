7121cbdfe5d4cd16b5ed7c6efb321da6a491852d
Kelli Libin <km.putnam2@gmail.com>
2024-12-06 15:36:21 -0700

    show dots on small load screens

--- src/app/main/loading.tsx
+++ src/app/main/loading.tsx
@@ -5,25 +5,38 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const Loading = () => {
   return (
-    <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
-      <div className="flex min-h-screen  w-full justify-center bg-customWhite px-4">
-        <div className="w-full h-full flex my-[180px] items-start justify-center">
-          <div className="flex items-center justify-center w-1/3">
-            <Skeleton width={400} height={400} />
-          </div>
+    <>
+      <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
+        <div className=" min-h-screen  w-full justify-center bg-customWhite px-4 hidden sm:flex">
+          <div className="w-full h-full flex my-[180px] items-start justify-center">
+            <div className="flex items-center justify-center w-1/3">
+              <Skeleton width={400} height={400} />
+            </div>
 
-          <div className="flex flex-col justify-center gap-10 w-1/2">
-            <h2>
-              <Skeleton count={1} height={180} />
-            </h2>
+            <div className="flex flex-col justify-center gap-10 w-1/2">
+              <h2>
+                <Skeleton count={1} height={180} />
+              </h2>
 
-            <p>
-              <Skeleton count={5} height={100} className="my-2" />
-            </p>
+              <p>
+                <Skeleton count={5} height={100} className="my-2" />
+              </p>
+            </div>
           </div>
         </div>
+      </SkeletonTheme>
+      <div className=" flex sm:hidden h-screen items-center justify-center bg-customWhite">
+        <div className="flex gap-6">
+          {[...Array(5)].map((_, index) => (
+            <div
+              key={index}
+              className="dot"
+              style={{ animationDelay: `${index * 0.2}s` }}
+            ></div>
+          ))}
+        </div>
       </div>
-    </SkeletonTheme>
+    </>
   );
 };
 
