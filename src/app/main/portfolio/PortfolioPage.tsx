6110153bad45c1bfb37135d761f9fce04356a264
Kelli Libin <km.putnam2@gmail.com>
2024-12-05 18:09:34 -0700

    hiding rainbows for now

--- src/app/main/portfolio/PortfolioPage.tsx
+++ src/app/main/portfolio/PortfolioPage.tsx
@@ -12,10 +12,11 @@ import CTA from "./CTA";
 
 const PortfolioPage = () => {
   return (
-    <div className="flex w-full sm:flex-row flex-col bg-customWhite min-h-[300vh] h-full ">
+    <div className="flex  !overflow-x-hidden sm:flex-row flex-col bg-customWhite h-full !overflow-hidden">
       {/* rainbows */}
-      <div className="absolute -top-[12vh] -left-[12%] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px]"></div>
-      <div className="hidden sm:absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px]"></div>
+      
+      {/* <div className="absolute -top-[12vh] -left-[12%] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px]"></div>
+      <div className="hidden sm:block sm:absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px]"></div> */}
 
 
       <div className="flex sm:w-1/3 w-full flex-col h-full items-end sm:justify-start relative sm:h-[300vh] 3xl:h-[230vh] sm:mt-[170px] mt-[5rem]">
@@ -26,7 +27,7 @@ const PortfolioPage = () => {
         </div>
       </div>
 
-      <div id="popup-container" className="w-full sm:w-2/3 flex bg-customWhite flex-col justify-center gap-4 items-center overflow-hidden mx-auto sm:mt-[180px]">
+      <div id="popup-container" className="w-full sm:w-2/3 flex z-[50] bg-customWhite sm:bg-transparent flex-col justify-center gap-4 items-center overflow-hidden mx-auto sm:mt-[180px]">
         {portfolioItems.map((item) => (
           <PortfolioCard
             key={item.id}
