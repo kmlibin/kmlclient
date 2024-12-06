33811b069a064af603ea4ce31fd20038f2e0db01
Kelli Libin <km.putnam2@gmail.com>
2024-12-06 15:06:10 -0700

    putting the rainbows in their own div

--- src/app/main/portfolio/PortfolioPage.tsx
+++ src/app/main/portfolio/PortfolioPage.tsx
@@ -12,22 +12,29 @@ import CTA from "./CTA";
 
 const PortfolioPage = () => {
   return (
-    <div className="flex  !overflow-x-hidden sm:flex-row flex-col bg-customWhite h-full !overflow-hidden">
+    <div className="flex sm:flex-row flex-col bg-customWhite h-full relative ">
       {/* rainbows */}
-      
-      {/* <div className="absolute -top-[12vh] -left-[12%] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px]"></div>
-      <div className="hidden sm:block sm:absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px]"></div> */}
-
+      <div className="h-full max-w-[100vw]  min-w-[105vw] xs:min-w-[100vw] overflow-x-hidden absolute">
+        <div className="absolute -top-[12vh] -left-[12%] rainbow-circle w-[900px] h-[900px] 3xl:w-[1100px] rounded-full 3xl:h-[1100px]"></div>
+        <div className="hidden sm:block sm:absolute -bottom-[10vh] -right-[10vw] rainbow-circle w-[500px] rounded-full h-[500px]"></div>
+      </div>
 
       <div className="flex sm:w-1/3 w-full flex-col h-full items-end sm:justify-start relative sm:h-[300vh] 3xl:h-[230vh] sm:mt-[170px] mt-[5rem]">
         <div className="sm:sticky top-[4vh]">
           <RollImage>
-            <Image alt="portfolio" src={myport} height={350} className="z-10" />
+            <Image
+              alt="portfolio"
+              src={myport}
+              className="z-10 h-[350px] w-auto"
+            />
           </RollImage>
         </div>
       </div>
 
-      <div id="popup-container" className="w-full sm:w-2/3 flex z-[50] bg-customWhite sm:bg-transparent flex-col justify-center gap-4 items-center overflow-hidden mx-auto sm:mt-[180px]">
+      <div
+        id="popup-container"
+        className="w-full sm:w-2/3 flex z-[50] bg-customWhite sm:bg-transparent flex-col justify-center gap-4 items-center mx-auto sm:mt-[180px]"
+      >
         {portfolioItems.map((item) => (
           <PortfolioCard
             key={item.id}
