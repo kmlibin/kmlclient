e4dd54c6936d2a575d0f37dbdce14e6b300ccd39
Kelli Libin <km.putnam2@gmail.com>
2024-12-07 09:52:35 -0700

    updated loading states so keleton shows dots on small screens

--- src/app/main/loading.tsx
+++ src/app/main/loading.tsx
@@ -6,25 +6,27 @@ import "react-loading-skeleton/dist/skeleton.css";
 const Loading = () => {
   return (
     <>
-      <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
-        <div className=" min-h-screen  w-full justify-center bg-customWhite px-4 hidden sm:flex">
-          <div className="w-full h-full flex my-[180px] items-start justify-center">
-            <div className="flex items-center justify-center w-1/3">
-              <Skeleton width={400} height={400} />
-            </div>
+      <div className="w-full h-full hidden sm:block">
+        <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
+          <div className=" min-h-screen  w-full justify-center bg-customWhite px-4 hidden sm:flex">
+            <div className="w-full h-full flex my-[180px] items-start justify-center">
+              <div className="flex items-center justify-center w-1/3">
+                <Skeleton width={400} height={400} />
+              </div>
 
-            <div className="flex flex-col justify-center gap-10 w-1/2">
-              <h2>
-                <Skeleton count={1} height={180} />
-              </h2>
+              <div className="flex flex-col justify-center gap-10 w-1/2">
+                <h2>
+                  <Skeleton count={1} height={180} />
+                </h2>
 
-              <p>
-                <Skeleton count={5} height={100} className="my-2" />
-              </p>
+                <p>
+                  <Skeleton count={5} height={100} className="my-2" />
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </SkeletonTheme>
+        </SkeletonTheme>
+      </div>
       <div className=" flex sm:hidden h-screen items-center justify-center bg-customWhite">
         <div className="flex gap-6">
           {[...Array(5)].map((_, index) => (
--- src/app/main/portfolio/loading.tsx
+++ src/app/main/portfolio/loading.tsx
@@ -5,25 +5,36 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const Loading = () => {
   return (
-    <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
-      <div className="flex min-h-screen  w-full justify-center bg-customWhite px-4">
+    <>
+      <div className="w-full h-full hidden sm:block">
+        <SkeletonTheme highlightColor="rgb(17, 17, 103, .25)">
+          <div className="flex min-h-screen  w-full justify-center bg-customWhite px-4">
+            <div className="w-full h-full flex my-[180px] items-start justify-center">
+              <div className="flex items-center justify-center w-1/3">
+                <Skeleton width={400} height={400} />
+              </div>
 
-        <div className="w-full h-full flex my-[180px] items-start justify-center">
-
-          <div className="flex items-center justify-center w-1/3">
-            <Skeleton width={400} height={400} />
+              <div className="flex flex-col justify-center gap-10 w-1/2">
+                <p>
+                  <Skeleton count={5} height={475} className="mb-10" />
+                </p>
+              </div>
+            </div>
           </div>
-
-
-          <div className="flex flex-col justify-center gap-10 w-1/2">
-
-            <p>
-              <Skeleton count={5} height={475} className="mb-10" />
-            </p>
-          </div>
+        </SkeletonTheme>
+      </div>
+      <div className=" flex sm:hidden h-screen items-center justify-center bg-customWhite">
+        <div className="flex gap-6">
+          {[...Array(5)].map((_, index) => (
+            <div
+              key={index}
+              className="dot"
+              style={{ animationDelay: `${index * 0.2}s` }}
+            ></div>
+          ))}
         </div>
       </div>
-    </SkeletonTheme>
+    </>
   );
 };
 
