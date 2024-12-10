89488eb1b7840ace66bb4f03e32edf28646447f7
Kelli Libin <km.putnam2@gmail.com>
2024-12-09 17:37:15 -0700

    adjust for small screens

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,67 @@
-
 import React from "react";
-import Image from "next/image";
+
+//images
 import notfound from "../../public/images/404.png";
+import Image from "next/image";
+//fonts
 import { fredoka, ibm, ibmBold } from "./utils/fonts";
+//routing
 import Link from "next/link";
 import paths from "./paths";
+//icons
 import { FaHouse } from "react-icons/fa6";
-
 
 const notFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-[80vh] w-full text-blackTextFont">
-      <div className="h-full w-full flex justify-center items-center flex-col">
-        <h2 className={`${fredoka.className} text-5xl py-4 tracking-wide`}>
+    <main
+      className="flex flex-col justify-center items-center min-h-[80vh] w-full text-blackTextFont"
+      aria-live="assertive"
+      id="main-content"
+    >
+      <div className=" sm:flex h-full w-full  justify-center items-center flex-col">
+        <h2
+          className={`${fredoka.className} text-5xl py-4 tracking-wide text-center`}
+        >
           Uh-Oh! This Page Didn&apos;t Make It...
         </h2>
-        <p className={`${ibm.className} text-xl my-4 flex`}>No worries - head to the <Link className={`${ibmBold.className} hover:text-customBlue duration-300`} href={paths.homePath()}><span className="flex items-center justify-center gap-2">&nbsp;<FaHouse /> homepage&nbsp;</span></Link> and start fresh!</p>
-        <Image src={notfound} alt="page not found" />
-        
+        <p className={`${ibm.className} hidden text-xl my-4 sm:flex`}>
+          No worries - head to the{" "}
+          <Link
+            aria-label="Go to homepage"
+            className={`${ibmBold.className} hover:text-customBlue duration-300`}
+            href={paths.homePath()}
+          >
+            <span className="flex items-center justify-center gap-2">
+              &nbsp;
+              <FaHouse /> homepage&nbsp;
+            </span>
+          </Link>{" "}
+          and start fresh!
+        </p>
+
+        <p
+          className={`${ibm.className} sm:hidden text-xl my-4 flex-col items-center justify-center text-center w-full`}
+        >
+          No worries - head to the{" "}
+          <Link
+            aria-label="Go to homepage"
+            className={`${ibmBold.className} hover:text-customBlue duration-300`}
+            href={paths.homePath()}
+          >
+            <span className="flex items-center justify-center gap-2">
+              &nbsp;
+              <FaHouse /> homepage&nbsp;
+            </span>
+          </Link>{" "}
+          and start fresh!
+        </p>
+
+        <Image
+          src={notfound}
+          alt="Illustration of a page not found, showing a broken link or missing page"
+        />
       </div>
-    </div>
+    </main>
   );
 };
 
