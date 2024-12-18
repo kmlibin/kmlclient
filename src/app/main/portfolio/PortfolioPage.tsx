8bef2776f2404d0f9c57adedb33ba70e01d89e06
Kelli Libin <km.putnam2@gmail.com>
2024-12-18 11:39:21 -0700

    add a delay to the portfolio items , only when roll image renders. seeing if that helps on small screens

--- src/app/main/portfolio/PortfolioPage.tsx
+++ src/app/main/portfolio/PortfolioPage.tsx
@@ -12,15 +12,12 @@ import PortfolioCard from "./PortfolioCube";
 import CTA from "./CTA";
 
 const PortfolioPage = () => {
-  const [renderPortfolioItems, setRenderPortfolioItems] = useState(false);
+  const [rollImageRendered, setRollImageRendered] = useState(false);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setRenderPortfolioItems(true);
-    }, 200);
+  const handleRollImageRenderComplete = () => {
+    setRollImageRendered(true);
+  };
 
-    return () => clearTimeout(timeout);
-  }, []);
   return (
     <div className="flex lg:flex-row flex-col bg-customWhite h-full relative ">
       {/* rainbows */}
@@ -31,12 +28,13 @@ const PortfolioPage = () => {
 
       <div className="flex lg:w-1/3 w-full flex-col h-full items-center lg:items-end lg:justify-start relative lg:h-[300vh] 3xl:h-[230vh] md:mt-[170px] mt-[5rem]">
         <div className="lg:sticky top-[4vh]">
-          <RollImage>
+        <RollImage onRenderComplete={handleRollImageRenderComplete}>
             <Image
               alt="portfolio"
               src={myport}
               className="z-10 h-[350px] w-auto"
               priority
+         
             />
           </RollImage>
         </div>
@@ -46,7 +44,7 @@ const PortfolioPage = () => {
         id="popup-container"
         className="w-full lg:w-2/3 flex z-[50] bg-customWhite  lg:bg-transparent flex-col justify-center gap-4 items-center mx-auto lg:mt-[180px]"
       >
-        {renderPortfolioItems
+        {rollImageRendered
           ? portfolioItems.map((item) => (
               <PortfolioCard
                 key={item.id}
--- src/components/RollImage.tsx
+++ src/components/RollImage.tsx
@@ -1,13 +1,23 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 //libraries
 import { Roll } from "react-awesome-reveal";
 
 interface ZoomHeadingProps {
   children: ReactNode;
   delay?: number;
+  onRenderComplete?: () => void;
 }
-const RollImage: React.FC<ZoomHeadingProps> = ({ delay, children }) => {
+const RollImage: React.FC<ZoomHeadingProps> = ({
+  delay,
+  children,
+  onRenderComplete,
+}) => {
+  useEffect(() => {
+    if (onRenderComplete) {
+      onRenderComplete();
+    }
+  }, [onRenderComplete]);
   return (
     <Roll duration={1200} delay={delay ? delay : 0} triggerOnce={true}>
       {children}
