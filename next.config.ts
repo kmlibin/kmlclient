f46634cff2d221f36a09e7c8dd8a8317ed87b439
Kelli Libin <km.putnam2@gmail.com>
2025-11-07 11:28:01 -0700

    removed resolveextensions

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
    turbopack: {
-    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
-    },
+  
+
   },
 };
 export default nextConfig
