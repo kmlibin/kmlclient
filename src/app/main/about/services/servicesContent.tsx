a168a480b559d72a71ea14ee4ffb2c6ab1fa22fc
Kelli Libin <km.putnam2@gmail.com>
2026-08-10 08:41:11 -0600

    adjust price for moment

--- src/app/main/about/services/servicesContent.tsx
+++ src/app/main/about/services/servicesContent.tsx
@@ -8,7 +8,7 @@ export const services = [
   {
     id: "website",
     title: "Custom Website Development",
-    price: "700",
+    price: "400",
     tagline: "Built for your business",
     accent: "#057ee6",
     lightAccent: undefined,
